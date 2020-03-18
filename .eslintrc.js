497c3002edc796f1de5d481de401141bab251a9c
Janderson Martins <jandersonmartins10@gmail.com>
2020-03-18 20:31:44 -0300

    Lint and initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jest'
+  ],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: require('./package.json').dependencies.react,
+    }
+  }
+}
