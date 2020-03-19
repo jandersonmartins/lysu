cd209c6c19f716b513e887ca4173d64b4b402d28
Janderson Martins <jandersonmartins10@gmail.com>
2020-03-19 20:41:47 -0300

    Config eslint with cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true
+    'jest/globals': true,
+    'cypress/globals': true
   },
   extends: [
     'plugin:react/recommended',
@@ -21,7 +22,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'jest'
+    'jest',
+    'cypress'
   ],
   rules: {
   },
