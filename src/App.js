144453d6e4bed202949278206d2095f5a1990124
Janderson Martins <jandersonmartins10@gmail.com>
2020-03-24 21:16:59 -0300

    Styled components theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
 import { Header } from './Header'
 import AppStyle from './App.style'
 
 const App = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <AppStyle />
     <Header />
-  </>
+  </ThemeProvider>
 )
 
 export default App
---
+++ src/theme/colors.js
@@ -0,0 +1,4 @@
+export default {
+  white: '#fff',
+  primary: '#0d47a1'
+}
