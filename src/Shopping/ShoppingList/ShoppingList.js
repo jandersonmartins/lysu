082904d89049ed4f4616b521e6d37aeb09ecf4b8
Janderson Martins <jandersonmartins10@gmail.com>
2020-04-02 10:49:21 -0300

    Create ShoppingList component

---
+++ src/Shopping/ShoppingList/ShoppingList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ShoppingList = ({ items }) => (
+  <ul>
+  </ul>
+)
+
+ShoppingList.propTypes = {
+  items: PropTypes.array
+}
+
+export default ShoppingList
