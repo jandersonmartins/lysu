19d70c3019ba32d83950553a20ed14d053170579
Janderson Martins <jandersonmartins10@gmail.com>
2020-04-28 09:12:10 -0300

    Translate Head component

--- src/Head/Head.js
+++ src/Head/Head.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { useTranslation } from 'react-i18next'
 
-const Head = () => (
-  <Helmet>
-    <title>Lysu - Online Shopping List</title>
-    <meta charSet="utf-8" />
-    <meta name="description" content="Lysu - Online Shopping List" />
+const Head = () => {
+  const { t } = useTranslation()
 
-    <meta property="og:locale" content="pt_BR" />
-    <meta property="og:title" content="Lysu - Online Shopping List" />
-    <meta property="og:site_name" content="Lysu" />
-    <meta property="og:description" content="Easily shopping list management with total values and quantity" />
-  </Helmet>
-)
+  return (
+    <Helmet>
+      <title>{t('siteDescription')}</title>
+      <meta charSet="utf-8" />
+      <meta name="description" content="Lysu - Online Shopping List" />
+
+      <meta property="og:locale" content="pt_BR" />
+      <meta property="og:title" content="Lysu - Online Shopping List" />
+      <meta property="og:site_name" content="Lysu" />
+      <meta property="og:description" content="Easily shopping list management with total values and quantity" />
+    </Helmet>
+  )
+}
 
 export default Head
